bbde99b2996adf0118bb1d2734da5f283b31d06c
Robert Saunders <rtsaunders19@gmail.com>
2017-09-25 13:43:39 -0400

    solved

--- arrayChange/arrayChange.js
+++ arrayChange/arrayChange.js
@@ -2,9 +2,6 @@ function arrayChange(inputArray) {
     var counter = 0;
 
     for (var i=0; i<inputArray.length; i++) {
-        if (inputArray[i]-inputArray[i+1]<-1) {
-            counter+=inputArray[i+1]-inputArray[i] - 1
-        }
         if (inputArray[i]-inputArray[i+1]>=0) {
             var addition = inputArray[i]-inputArray[i+1] + 1
             var newValue = inputArray[i+1]+addition;
